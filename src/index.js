2446b57262c4d2000d1fb28394f19b0101ceac36
Victor Pazaran <victor_hals@hotmail.com>
2021-06-03 19:14:34 -0500

    adding router logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+import router from './routes/index';
+
 console.log('test');
 
-let test = [2,4,5,7,8,9,7,1,4];
-
-test.map((number) => number+5);
+window.addEventListener('load', router);
+window.addEventListener('hashchange', router);
