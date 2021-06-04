eeb28a8c745f13edab0afbb48d6cdd5333883b45
Victor Pazaran <victor_hals@hotmail.com>
2021-06-04 11:54:35 -0500

    getting the character info from the api

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,20 +1,25 @@
 import getData from '../utils/getData';
+import getHash from '../utils/getHash'
 
 const Character = async () => {
+   let characterId = getHash();
+   let character = await getData(characterId)
+
    const view = `
       <div class="Characters">
-         <article class="Characters-card">
-            <img src="image" alt="name">
-            <h2>name</h2>
-         </article>
-         <article class="Characters-card">
-            <h3>Episodes:</h3>
-            <h3>Status:</h3>
-            <h3>Species:</h3>
-            <h3>Gender:</h3>
-            <h3>Origin:</h3>
-            <h3>Last known location:</h3>
-         </article>
+
+            <article class="Characters-card">
+               <img src="${character.image}" alt="${character.name}">
+               <h2>${character.name}</h2>
+            </article>
+            <article class="Characters-card">
+               <h3>Episodes: ${character.episode.length}</h3>
+               <h3>Status: ${character.status}</h3>
+               <h3>Species ${character.species}</h3>
+               <h3>Gender: ${character.gender}</h3>
+               <h3>Origin: ${character.origin.name}</h3>
+               <h3>Last known location: ${character.location.name}</h3>
+            </article>
       </div>
    `
 
